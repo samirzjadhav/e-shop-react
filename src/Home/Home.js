9f98c7f402e0866cbfcfb20615e59a80d2edae32
Samir Jadhav <samirzjadhav@gmail.com>
2024-02-14 22:09:19 +0530

    feat: added product

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Home.css";
 
+import Product from "../Product/Product.js";
+
 const Home = () => {
   return (
     <div className="home">
@@ -8,8 +10,22 @@ const Home = () => {
         <img
           src="https://www.x-cart.com/wp-content/uploads/2019/01/ecommerce-768x278.jpg"
           alt=""
-          className="home-image w-full"
+          className="home-image"
         />
+
+        <div className="home-row">
+          <Product />
+          <Product />
+        </div>
+        <div className="home-row">
+          <Product />
+          <Product />
+          <Product />
+        </div>
+        <div className="home-row">
+          <Product />
+          <Product />
+        </div>
       </div>
     </div>
   );
--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "./Product.css";
 
 const Product = () => {
-  return <div></div>;
+  return (
+    <div className="product">
+      <div className="product-info">
+        <p>Title</p>
+        <p className="product-price">$39</p>
+        <div className="product-rating">⭐️⭐️⭐️⭐️⭐️</div>
+      </div>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
+        alt=""
+      />
+      <button>Add To Basket</button>
+    </div>
+  );
 };
 
 export default Product;
