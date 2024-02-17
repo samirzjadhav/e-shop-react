f3e0933dda0e2f6aa2680e8e17beeda78d2cd795
Samir Jadhav <samirzjadhav@gmail.com>
2024-02-17 23:15:12 +0530

    feat: added react dom router and switch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import "./App.css";
+import Checkout from "./Checkout/Checkout.js";
 import Header from "./Header/Header.js";
 import Home from "./Home/Home.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Checkout/Checkout.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Checkout.css";
+
+const Checkout = () => {
+  return <div>hello world</div>;
+};
+
+export default Checkout;
