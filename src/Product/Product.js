d1a86e808bec91a3dfa20c227d2ad2d4fda67e48
Samir Jadhav <samirzjadhav@gmail.com>
2024-02-22 11:59:07 +0530

    feat: maked reducer for cart

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "../StateProvider/StateProvider";
 
 const Product = ({ id, title, image, price, rating }) => {
+  const [state, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
   return (
     <div className="product">
       <div className="product-info">
@@ -19,7 +33,7 @@ const Product = ({ id, title, image, price, rating }) => {
         </div>
       </div>
       <img src={image} className="flex items-center text-center" />
-      <button>Add To Basket</button>
+      <button onClick={addToBasket}>Add To Basket</button>
     </div>
   );
 };
---
+++ src/Reducer/Reducer.js
@@ -0,0 +1,18 @@
+// here we define all the application level state and define action to make the changes to state
+
+export const intilialState = {
+  basket: [],
+};
+
+// Selector
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_BASKET":
+      return {
+        ...state,
+        basket: [...state.basket, action.item],
+      };
+  }
+};
+export default reducer;
