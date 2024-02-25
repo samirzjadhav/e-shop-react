0758e520764ed81fce3559d8be0292f3d6c8486f
Samir Jadhav <samirzjadhav@gmail.com>
2024-02-25 12:27:30 +0530

    feat: added subtotal on cart

--- src/Subtotal/Subtotal.js
+++ src/Subtotal/Subtotal.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "../StateProvider/StateProvider";
+import { getBasketTotal } from "../Reducer/Reducer";
 
 const Subtotal = () => {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="subtotal">
       <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 item): <strong>$0</strong>
+              Subtotal ({basket.length} item): <strong>{value}</strong>
             </p>
             <small className="subtotal-gift">
               <input type="checkbox" /> this order contains a gift
@@ -17,7 +20,7 @@ const Subtotal = () => {
           </>
         )}
         decimalScale={2}
-        value={0}
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
